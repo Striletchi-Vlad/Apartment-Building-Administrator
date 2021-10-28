4cee89a081b81fe0a808ac5e454a20b2c4e19e2b
VladStriletchi <vladstriletchi@gmail.com>
2021-10-28 18:26:01 +0300

    made it the most beautiful thing ever

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 from testing.tests import run_all_tests
 from business.expense_service import business_start_console
 
+f = open('ART.txt', 'r')
+art = f.read()
+print(art)
+f.close()
 
 run_all_tests()
 business_start_console()
